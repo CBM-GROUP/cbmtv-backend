15c753095dd4f8fee43af79cf336831aaa78ddcc
louis kalinzi <klouisphillip@gmail>
2025-10-29 18:41:40 +0300

    Update ALLOWED_HOSTS in settings.py for production deployment
    - Changed the production host from "cbmtv-backend-production.up.railway.app" to "web-production-03b3b.up.railway.app".
    - Regenerated bytecode for updated settings.

--- cbmtv/settings.py
+++ cbmtv/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-l-lxx-*u2my&r*8p66%m*ppf+shl8s_@cz3(njf7xabcj2dg)(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-    "cbmtv-backend-production.up.railway.app",
+    "web-production-03b3b.up.railway.app",
     "127.0.0.1",
 ]
 CSRF_TRUSTED_ORIGINS = [
